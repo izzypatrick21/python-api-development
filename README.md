df5cbe0bf4e7024b2ab37e37a7335867683ed81b
Patrick Isaiah <patrick21isaiah@gmail.com>
2021-12-18 12:10:04 +0100

    delete request

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # python-api-development
 
+Python API development with FastAPI - social media posts
+
 ![alt text](./crud_image.png)
--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from typing import Optional
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status, HTTPException
 from fastapi.params import Body
 from pydantic import BaseModel
+from random import randrange
+
+from starlette.status import HTTP_404_NOT_FOUND
 
 
 app = FastAPI()
@@ -14,18 +17,53 @@ class Post(BaseModel):
     rating: Optional[int] = None
 
 
+my_posts = [
+    {
+        "title": "title of post 1",
+        "content": "content of post 1",
+        "id": 1
+    },
+    {
+        "title": "title of post 2",
+        "content": "content of post 2",
+        "id": 2
+    },
+    {
+        "title": "title of post 3",
+        "content": "content of post 3",
+        "id": 3
+    }
+]
+
+
+def find_post(id):
+    for post in my_posts:
+        if post["id"] == id:
+            return post
+
+
 @app.get("/")
 def root():
-    return {"message": "Hello World, welcome to python api"}
+    return {"message": "Welcome to python api development with FastAPI"}
 
 
 @app.get("/posts")
 def get_posts():
-    return {"data": "This is your posts"}
+    return {"data": my_posts}
 
 
-@app.post('/posts')
-def createposts(post: Post):
-    print(post)
-    print(post.dict())
-    return {"post": post}
+@app.post('/posts', status_code=status.HTTP_201_CREATED)
+def create_posts(post: Post):
+    post_dict = post.dict()
+    post_dict['id'] = randrange(0, 10000)
+    my_posts.append(post_dict)
+    return {"post": post_dict}
+
+
+@app.get("/posts/{id}")
+def get_post(id: int, response: Response):
+    post = find_post(id)
+    if not post:
+        raise HTTPException(status_code=HTTP_404_NOT_FOUND,
+                            detail=f'post with id {id} was not found')
+    return {"post_detail": post}
